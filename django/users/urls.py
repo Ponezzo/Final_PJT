d3bbff6a2b1701c697e19a230af1e7a1b6bd6b12
taegeon <taegeonpark97@gmail.com>
2024-11-19 10:12:44 +0900

    1

--- django/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-appname = 'users'
-urlpatterns = [
-    path('signup/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-]
